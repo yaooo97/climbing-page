859a8f6873fdb05b4125fd8989bb813935d776a3
Yao Wang <coconutwhite@Yaos-MacBook-Pro.local>
2024-09-25 23:16:00 -0700

    adding search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
+  const items = ['Apple', 'Banana', 'Orange', 'Pineapple', 'Mango'];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search Example</h1>
+      <SearchBar data={items} />
     </div>
   );
 }
